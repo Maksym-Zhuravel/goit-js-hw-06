1f02de205e5c668af2b99b78caf5c1905d5d27fa
Maksym-Zhuravel <allwegotizus1@gmail.com>
2023-03-19 23:18:09 +0200

    Create js

---
+++ js/task-01.js
@@ -0,0 +1,7 @@
+const liItem = document.querySelectorAll('.item')
+console.log(`Number of categories: ${liItem.length}`)
+liItem.forEach(element => {
+    console.log(`Category: ${element.firstElementChild.textContent}`)
+    console.log(`Elements: ${element.lastElementChild.children.length}`)
+})
+
---
+++ js/task-02.js
@@ -0,0 +1,19 @@
+const ingredients = [
+  'Potatoes',
+  'Mushrooms',
+  'Garlic',
+  'Tomatos',
+  'Herbs',
+  'Condiments',
+];
+
+const ulItem = document.querySelector('#ingredients')
+console.log(ulItem)
+
+ingredients.forEach(ingredient => {
+  const liItem = document.createElement("li");
+  liItem.classList.add('item')
+  liItem.textContent = `${ingredient}`
+  ulItem.append(liItem)
+})
+
