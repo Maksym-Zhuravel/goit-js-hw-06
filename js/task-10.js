05148c02c30cc4d0452736e0e97a9e5150a00169
Maksym-Zhuravel <allwegotizus1@gmail.com>
2023-03-23 20:59:01 +0200

    update JS

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,20 @@
+const inputEl = document.querySelector('#controls input')
+const boxDiv = document.getElementById('boxes')
+const createBtn = document.querySelector('[data-create]')
+const destroyBtn = document.querySelector('[data-destroy]')
+
+createBtn.addEventListener('click', onAmountValue)
+
+function onAmountValue() {
+  const amount = inputEl.value
+  console.log(amount)
+}
+
+function onCreateDiv(amount) {
+  const newDiv = document.createElement('div')
+  console.log(newDiv)
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
